e6d17394076d0988becaf8607362d35235a755db
Pablo Jurado <pjurado85@gmail.com>
2017-06-10 11:15:21 -0500

    split gameStatus in diff functions so we all can work on it

--- connect4-lib.js
+++ connect4-lib.js
@@ -15,6 +15,47 @@ const emptyBoard = [
 // - returns null if the board passed in is not valid
 function gameStatus (board) {
   // TODO: write me
+
+  if (!isGoodInput(board)) return null
+
+  // if there is a winner should return 'winner_red' || 'winner_yellow'
+  if (checkRowWinner()) return checkRowWinner()
+  if (checkColumnWinner()) return checkColumnWinner()
+  if (checkDiagonalWinner()) return checkDiagonalWinner()
+
+  // if there is no winners and board is full return tie
+  if (isBoardFull) return 'tie'
+
+  // if there is no winners and board is NOT full return 'in_progress'
+  if (!isBoardFull) return 'in_progress'
+}
+
+function isBoardFull () {
+  // TODO: return true or false
+}
+
+function isGoodInput (input) {
+// gameStatus should return null if it receives no arguments
+// gameStatus should return null if a game board is not passed to it
+// gameStatus should return null if a game board is not passed to it
+// gameStatus should return null if a game board is not passed to it
+// gameStatus should return null if a game board is not passed to it
+// gameStatus should return null if a game board is not passed to it
+}
+
+function checkRowWinner () {
+  // TODO: should return null || 'winner_red' || 'winner_yellow'
+
+}
+
+function checkColumnWinner () {
+  // TODO: should return null || 'winner_red' || 'winner_yellow'
+
+}
+
+function checkDiagonalWinner () {
+  // TODO: should return null || 'winner_red' || 'winner_yellow'
+
 }
 
 // returns true or false
